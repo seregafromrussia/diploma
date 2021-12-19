d2455f69eb1181da6e1ed78c3c89b60ec91ef00e
Vasilev Sergey <sergey21va@mail.ru>
2021-12-20 01:38:37 +0300

    Add the LSTM model with an ability to predict one feature

---
+++ notebook/.ipynb_checkpoints/test.py
@@ -0,0 +1,6 @@
+plot_predictions_on_whole_interval(predicted_array=predicted_stock_price,
+                                   from_test='2021',
+                                   to_train='2020',
+                                   title='FXCN stock price',
+                                   date_column='date',
+                                   target_column='value_FXCN')
